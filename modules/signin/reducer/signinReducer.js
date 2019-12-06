eac02661ef2d57586a75d2801134b30191b6c78e
Riyaz <ahmed.riyaz94@gmail.com>
2019-12-06 21:16:00 +0530

    fixes login issue

--- modules/signin/reducer/signinReducer.js
+++ modules/signin/reducer/signinReducer.js
@@ -14,39 +14,39 @@ const initialState = {
 
 export default function signinReducer(state = initialState, {type, payload}) {
 switch (type) {
-  case SIGN_IN_REQUEST:
-    return {
-      ...state,
-    };
-  
   case SIGN_IN_SUCCESS:
-    return {
-      ...state,
-      userDetails: payload.data.user_details,
-      userToken: payload.data.user_token,
-      isUserSignedIn: true,
-    };
-  
-  case SIGN_IN_FAILURE:
-    return {
-      ...state,
-    };
+    if (payload.code == 200 || payload.code == 201) {
+      return {
+        ...state,
+        userDetails: payload.data.user_details,
+        userToken: payload.data.user_token,
+        isUserSignedIn: true,
+      };  
+    }
+
+    return { ...state }
   
   case SET_USER_DATA:
-
     return {
       ...state,
       userDetails: payload.user_details,
       userToken: payload.user_token,
       isUserSignedIn: true,
-    };
-  case LOGOUT_SUCCESS: 
-    return {
-      ...state,
-      userDetails: null,
-      userToken: null,
-      isUserSignedIn: false,
+    };  
+ 
+  case LOGOUT_SUCCESS:
+
+    if (payload.code == 200 || payload.code == 201) {
+      return {
+        ...state,
+        userDetails: null,
+        userToken: null,
+        isUserSignedIn: false,
+      }
     }
+
+    return { ...state }
+  
   default:
     return state;
   }
