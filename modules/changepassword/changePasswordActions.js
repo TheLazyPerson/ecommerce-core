9d24b710b80f6ace75c3507195c6176ba2aea1e4
Kunal Pardeshi <kunalpardeshi280@gmail.com>
2019-12-10 23:55:17 +0530

    Added change password module

---
+++ modules/changepassword/changePasswordActions.js
@@ -0,0 +1,22 @@
+import { RSAA } from "redux-api-middleware";
+import { API_ROOT } from "Core/constants";
+import {
+  POST_CHANGE_PASSWORD_REQUEST,
+  POST_CHANGE_PASSWORD_SUCCESS,
+  POST_CHANGE_PASSWORD_FAILURE
+} from "./constants";
+
+export function changePasswordAction(changePasswordObject) {
+  return {
+    [RSAA]: {
+      endpoint: `${API_ROOT}/customer/change_password`,
+      method: "POST",
+      body: JSON.stringify(changePasswordObject),
+      types: [
+        POST_CHANGE_PASSWORD_REQUEST,
+        POST_CHANGE_PASSWORD_SUCCESS,
+        POST_CHANGE_PASSWORD_FAILURE
+      ]
+    }
+  };
+}
---
+++ modules/changepassword/changePasswordReducer.js
@@ -0,0 +1,29 @@
+import {
+  POST_CHANGE_PASSWORD_REQUEST,
+  POST_CHANGE_PASSWORD_SUCCESS,
+  POST_CHANGE_PASSWORD_FAILURE
+} from "./constants";
+
+const initialState = {};
+
+export default function changePasswordReducer(
+  state = initialState,
+  { type, payload }
+) {
+  switch (type) {
+    case POST_CHANGE_PASSWORD_REQUEST:
+      return {
+        ...state
+      };
+    case POST_CHANGE_PASSWORD_SUCCESS:
+      return {
+        ...state
+      };
+    case POST_CHANGE_PASSWORD_FAILURE:
+      return {
+        ...state
+      };
+    default:
+      return state;
+  }
+}
