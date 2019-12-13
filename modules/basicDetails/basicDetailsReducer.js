f3267f159592df9d34b781a7b85c36236ab1c5b6
Riyaz <ahmed.riyaz94@gmail.com>
2019-12-13 20:47:32 +0530

    created basic details modules

---
+++ modules/basicDetails/basicDetailsReducer.js
@@ -0,0 +1,11 @@
+const initialState = {};
+
+export default function basicDetailsReducer(
+  state = initialState,
+  { type, payload }
+) {
+  switch (type) {
+    default:
+      return state;
+  }
+}
