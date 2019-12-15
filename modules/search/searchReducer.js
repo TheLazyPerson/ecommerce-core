6005d3dd14f6ac5f5f535bc4a120bd6afabfdc54
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2019-12-15 22:04:29 +0530

    handles wishlist actions in search reducer

--- modules/search/searchReducer.js
+++ modules/search/searchReducer.js
@@ -1,6 +1,11 @@
 import {
 GET_SEARCH_SUCCESS,
 } from "./searchConstants";
+import {
+  POST_ADD_TO_WISHLIST_SUCCESS,
+  GET_REMOVE_FROM_WISHLIST_SUCCESS
+} from 'Core/modules/wishlist/wishlistConstants';
+import map from 'lodash/map';
 
 const initialState = {
   productList: [],
@@ -9,7 +14,7 @@ const initialState = {
 
 export default function searchReducer(
   state = initialState,
-  { type, payload }
+  { type, payload, meta }
 ) {
   switch (type) {
 
@@ -19,8 +24,33 @@ export default function searchReducer(
         productList: payload.data.products,
         exhibitionList: payload.data.exhibitions
       };
+    case POST_ADD_TO_WISHLIST_SUCCESS:
+      
+      return {
+        ...state,
+        productList: updateProductFromList(state.productList, meta.product_id, {is_wishlisted: true})
+      };
 
+    case GET_REMOVE_FROM_WISHLIST_SUCCESS:
+      return {
+        ...state,
+        productList: updateProductFromList(state.productList, meta.product_id, {is_wishlisted: false})
+      }
     default:
       return state;
   }
 }
+
+const updateProductFromList = (productList, productId, updateObject) => {
+  return map(productList, product => {
+    if(product.id == productId) {
+      return {
+        ...product,
+        ...updateObject,
+      }
+    }
+
+    return product;
+  });
+}
+
