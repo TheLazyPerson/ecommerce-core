396ec972df5d14a137e23e122330d82e3a6b8a93
Riyaz <ahmed.riyaz94@gmail.com>
2020-01-08 16:16:43 +0530

    created basic module

---
+++ modules/basic/basicReducer.js
@@ -0,0 +1,19 @@
+import {
+  GET_BASIC_ALL_SUCCESS,
+} from "./constants";
+
+const initialState = {
+  basicData: {}
+};
+
+export default function basicReducer(state = initialState, { type, payload }) {
+  switch (type) {
+    case GET_BASIC_ALL_SUCCESS:
+      return {
+        ...state,
+        basicData: payload.data,
+      }
+    default:
+      return state;
+  }
+}
