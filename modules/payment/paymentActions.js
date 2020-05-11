19d09a0e928526210bb2dea557a1bce6a5d6ca28
Om Komawar <omkomawar222@gmail.com>
2020-05-11 10:34:57 +0530

    updated payment action

--- modules/payment/paymentActions.js
+++ modules/payment/paymentActions.js
@@ -1,28 +1,31 @@
-import {RSAA} from 'redux-api-middleware';
-import {API_ROOT} from 'Core/constants';
+import { RSAA } from "redux-api-middleware";
+import { API_ROOT } from "Core/constants";
 import {
   GET_PAYMENT_REQUEST,
   GET_PAYMENT_SUCCESS,
   GET_PAYMENT_FAILURE,
-} from './constants';
+} from "./constants";
 
-export function getPaymentAction(orderId, paymentType = 'customer_order') {
+export function getPaymentAction(orderId, paymentType = "customer_order") {
   return {
     [RSAA]: {
-      endpoint: `${API_ROOT}/payment/test/url?order_id=${orderId}&payment_type=${paymentType}`,
-      method: 'GET',
+      endpoint: `${API_ROOT}/payment/order`,
+      method: "POST",
+      body: JSON.stringify({
+        order_id: orderId,
+      }),
       types: [
         {
           type: GET_PAYMENT_REQUEST,
-          meta: {showLoader: true, showLoaderMobile: true},
+          meta: { showLoader: true, showLoaderMobile: true },
         },
         {
           type: GET_PAYMENT_SUCCESS,
-          meta: {showLoader: false, showLoaderMobile: false},
+          meta: { showLoader: false, showLoaderMobile: false },
         },
         {
           type: GET_PAYMENT_FAILURE,
-          meta: {showLoader: false, showLoaderMobile: false},
+          meta: { showLoader: false, showLoaderMobile: false },
         },
       ],
     },
