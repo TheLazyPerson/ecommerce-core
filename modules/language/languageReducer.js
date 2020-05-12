f487c0bfdb7053908cf3857d7ebd78b768b1c84a
Kunal Pardeshi <kunal.pardeshi@harbingergroup.com>
2020-05-12 10:19:24 +0530

    RTL state added to language reducer

--- modules/language/languageReducer.js
+++ modules/language/languageReducer.js
@@ -1,20 +1,17 @@
-import {
-  SET_LANGUAGE,
-} from './constants';
+import {SET_LANGUAGE} from './constants';
 
 const initialState = {
-  languageCode: ''
+  languageCode: '',
+  isRTL: false,
 };
 
-export default function languageReducer(
-  state = initialState,
-  { type, payload }
-){
+export default function languageReducer(state = initialState, {type, payload}) {
   switch (type) {
     case SET_LANGUAGE:
       return {
         ...state,
         languageCode: payload,
+        isRTL: payload === 'ar' ? true : false,
       };
     default:
       return state;
